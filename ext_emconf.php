78fdd3c58050d9cdd237252bce895eff7deb96f0
Jigal van Hemert <jigal.van.hemert@typo3.org>
2016-05-14 11:00:45 +0200

    [TASK] Cleaned some CSS, updated core dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '7.0.8',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '7.6.0-8.0.99',
+			'typo3' => '7.6.0-8.1.99',
 		),
 		'conflicts' => array(
 		),
