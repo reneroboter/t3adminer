b0624a1f0f7b0da3f7a951aab646d638bd7c2dc1
Jigal van Hemert <jigal.van.hemert@typo3.org>
2019-03-17 08:50:48 +0100

    [FEATURE] PHP 7.3 tested, adminer 4.7.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,11 +11,11 @@ $EM_CONF[$_EXTKEY] = [
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'clearCacheOnLoad' => 0,
-	'version' => '9.2.1',
+	'version' => '9.3.0',
 	'constraints' => [
 		'depends' => [
 			'typo3' => '9.5.0-9.5.999',
-            'php' => '7.2.0-7.2.999',
+            'php' => '7.2.0-7.3.999',
         ],
 		'conflicts' => [],
 		'suggests' => [],
